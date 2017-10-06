d37fc7003bdcbf3c1bd37174eddbb5da19f9e64c
Michael Harootoonyan <michaelharootoonyan@yahoo.com>
2017-10-06 00:32:17 -0700

    too many modifcations needed to be made to test files so reverted back to non conventional naming

--- sql/elegantBookstore.sql
+++ sql/elegantBookstore.sql
@@ -11,7 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `mvc`
+-- Database: `elegantBookstore`
 --
 
 -- --------------------------------------------------------
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `authors` (
-  `id` int(9) NOT NULL,
+  `author_id` int(9) NOT NULL,
   `author_name` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -29,7 +29,7 @@ CREATE TABLE `authors` (
 -- Dumping data for table `authors`
 --
 
-INSERT INTO `authors` (`id`, `author_name`) VALUES
+INSERT INTO `authors` (`author_id`, `author_name`) VALUES
 (1, 'Steven S. Skiena'),
 (2, 'Miguel A. Revilla'),
 (3, 'Gayle Laakmann McDowell'),
@@ -44,7 +44,7 @@ INSERT INTO `authors` (`id`, `author_name`) VALUES
 --
 
 CREATE TABLE `books` (
-  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
   `title` varchar(250) NOT NULL,
   `description` text NOT NULL,
   `genre_id` int(9) DEFAULT NULL
@@ -54,7 +54,7 @@ CREATE TABLE `books` (
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `title`, `description`, `genre_id`) VALUES
+INSERT INTO `books` (`book_id`, `title`, `description`, `genre_id`) VALUES
 (1, 'The Algorithm Design Manual', 'This newly expanded and updated second edition of the best-selling classic continues to take the \"mystery\" out of designing algorithms, and analyzing their efficacy and efficiency. Expanding on the first edition, the book now serves as the primary textbook of choice for algorithm design courses while maintaining its status as the premier practical reference guide to algorithms for programmers, researchers, and students.', 1),
 (2, 'Cracking the Coding Interview: 189 Programming Questions and Solutions', 'I am not a recruiter. I am a software engineer. And as such, I know what it\'s like to be asked to whip up brilliant algorithms on the spot and then write flawless code on a whiteboard. I\'ve been through this as a candidate and as an interviewer. ', 1),
 (3, 'Programming Challenges: The Programming Contest Training Manual', 'There are many distinct pleasures associated with computer programming. Craftsm- ship has its quiet rewards, the satisfaction that comes from building a useful object and making it work. Excitement arrives with the ?ash of insight that cracks a previously intractable problem. The spiritual quest for elegance can turn the hacker into an artist. Therearepleasuresinparsimony,insqueezingthelastdropofperformanceoutofclever algorithms ', 1),
@@ -92,14 +92,14 @@ INSERT INTO `books_authors` (`book_id`, `author_id`) VALUES
 
 CREATE TABLE `genres` (
   `id` int(9) NOT NULL,
-  `name` varchar(250) DEFAULT NULL
+  `genre_name` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `genres`
 --
 
-INSERT INTO `genres` (`id`, `name`) VALUES
+INSERT INTO `genres` (`id`, `genre_name`) VALUES
 (1, 'Educational'),
 (2, 'Mystery'),
 (3, 'Romance');
@@ -112,13 +112,13 @@ INSERT INTO `genres` (`id`, `name`) VALUES
 -- Indexes for table `authors`
 --
 ALTER TABLE `authors`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`author_id`);
 
 --
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`book_id`);
 
 --
 -- Indexes for table `genres`
@@ -134,12 +134,12 @@ ALTER TABLE `genres`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `author_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `genres`
 --
