00d81141115bc9b4e8c289b7c448e4ad397a13d7
mharoot <michaelharootoonyan@yahoo.com>
2017-10-15 17:09:09 -0700

    added customers and orders table

--- sql/elegantBookstore.sql
+++ sql/elegantBookstore.sql
@@ -1,14 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 30, 2017 at 05:46 AM
--- Server version: 5.6.35
--- PHP Version: 7.1.6
+-- Host: 127.0.0.1
+-- Generation Time: Oct 16, 2017 at 02:07 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `elegantbookstore`
@@ -55,7 +63,7 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`book_id`, `title`, `description`, `genre_id`) VALUES
-(1, 'The Algorithm Design Manual', 'This newly expanded and updated second edition of the best-selling classic continues to take the \"mystery\" out of designing algorithms, and analyzing their efficacy and efficiency. Expanding on the first edition, the book now serves as the primary textbook of choice for algorithm design courses while maintaining its status as the premier practical reference guide to algorithms for programmers, researchers, and students.', 1),
+(1, 'The Algorithm Design Manual', 'Cool book dude!', 1),
 (2, 'Cracking the Coding Interview: 189 Programming Questions and Solutions', 'I am not a recruiter. I am a software engineer. And as such, I know what it\'s like to be asked to whip up brilliant algorithms on the spot and then write flawless code on a whiteboard. I\'ve been through this as a candidate and as an interviewer. ', 1),
 (3, 'Programming Challenges: The Programming Contest Training Manual', 'There are many distinct pleasures associated with computer programming. Craftsm- ship has its quiet rewards, the satisfaction that comes from building a useful object and making it work. Excitement arrives with the ?ash of insight that cracks a previously intractable problem. The spiritual quest for elegance can turn the hacker into an artist. Therearepleasuresinparsimony,insqueezingthelastdropofperformanceoutofclever algorithms ', 1),
 (4, 'Data Structures and Algorithms in Java', 'The design and analysis of efficient data structures has long been recognized as a key component of the Computer Science curriculum. Goodrich, Tomassia and Goldwasser\'s approach to this classic topic is based on the object-oriented paradigm as the framework of choice for the design of data structures. For each ADT presented in the text, the authors provide an associated Java interface. Concrete data structures realizing the ADTs are provided as Java classes implementing the interfaces. The Java code implementing fundamental data structures in this book is ', 1);
@@ -87,6 +95,27 @@ INSERT INTO `books_authors` (`book_id`, `author_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(128) UNSIGNED NOT NULL,
+  `name` varchar(128) DEFAULT NULL,
+  `address` varchar(128) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `address`) VALUES
+(1, 'chad', '232 b'),
+(2, 'chris', '123 a'),
+(3, 'mike', '234 c');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `genres`
 --
 
@@ -103,6 +132,27 @@ INSERT INTO `genres` (`id`, `genre_name`) VALUES
 (1, 'Educational'),
 (2, 'Mystery'),
 (3, 'Romance');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `amount` int(10) UNSIGNED DEFAULT NULL,
+  `customer_id` int(10) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `amount`, `customer_id`) VALUES
+(1, 125, 1),
+(2, 225, 2),
+(3, 300, 3);
 
 --
 -- Indexes for dumped tables
@@ -135,13 +185,20 @@ ALTER TABLE `genres`
 --
 ALTER TABLE `authors`
   MODIFY `author_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
   MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `genres`
 --
 ALTER TABLE `genres`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
