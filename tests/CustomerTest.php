aa4795af7b6b1eeccf51a5619848185da46fc6d3
mharoot <michaelharootoonyan@yahoo.com>
2017-10-21 16:24:57 -0700

    Elegant is finally on its way to morphing into an ORM

--- tests/CustomerTest.php
+++ tests/CustomerTest.php
@@ -8,7 +8,35 @@ include_once('model/Customer.php');
 
 class CustomerTest extends TestCase
 {
-    public function test_testing_is_ready(){$this->assertTrue(TRUE);}
+    public function test_the_table_name_property_inhertited_from_Model_before_Customer_instantiated()
+    {
+        $class_name = "Customer";
+        
+        $class_vars = get_class_vars ( $class_name );
+        foreach ($class_vars as $property_name => $value) 
+        {
+            echo "$property_name : $value\n";
+            switch($property_name)
+            {
+                case 'table_name':
+                    $this->assertEquals('table_name',$property_name);
+                    break;
+                case 'id':
+                    $this->assertEquals('id',$property_name);
+                    break;
+                case 'address':
+                    $this->assertEquals('address', $property_name);
+                    break;
+                case 'name':
+                    $this->assertEquals('name', $property_name);
+                    break;
+            }
+            
+        }
+
+    }
+    
+
 
     
     public function test_one_to_many_get_all_function()
@@ -20,6 +48,26 @@ class CustomerTest extends TestCase
         $this->assertTrue(sizeof($result) > 0);
     }
 
+
+    public function test_orm_properties_of_an_instance_of_customer_model()
+    {
+        $customer = new Customer();
+        $customer->address = "1642 Daily Circle Drive, Glendale Ca 91208";
+        $customer->name    = "Craig Walker";
+        // $customer->save();
+
+        // testing
+        $class_vars = $customer->getChildProps();
+        var_dump($class_vars);
+        echo $class_vars['name']."\n";
+        echo $class_vars['address'];
+        // foreach ($class_vars as $property_name => $value) 
+        // {
+        //     echo "$property_name : $value\n";
+        // }
+
+        $this->assertTrue(true);
+    }
     
 }
 ?>
